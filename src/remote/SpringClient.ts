1cf35fa441ca5b94d44e6df2121fc994c0da859d
hwu5542 <hwu5542@gmail.com>
2021-11-04 15:22:56 -0500

    done with most part and deploy

--- src/remote/SpringClient.ts
+++ src/remote/SpringClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const SpringClient = axios.create({
-    //baseURL:'http://ec2-3-144-248-75.us-east-2.compute.amazonaws.com:8081/teatimeexpress/',
-    baseURL:'http://localhost:8081/teatimeexpress/',
+    baseURL:'http://ec2-3-143-204-100.us-east-2.compute.amazonaws.com:8081/teatimeexpress/',
+    // baseURL:'http://localhost:8081/teatimeexpress/',
     headers: {
         'Content-Type':'application/json'
     }
